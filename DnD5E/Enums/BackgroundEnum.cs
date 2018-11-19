91bbec632e7d79b203b391603f0484fc0c59f658
elukavsky <elukavsky@anexinet.com>
2018-11-19 11:19:37 -0500

    Created Background, Class, and Race enums

---
+++ DnD5E/Enums/BackgroundEnum.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnD5E.Enums
+{
+
+    public enum BackgroundEnum
+    {
+        [Description("Acolyte")]
+        acolyte,
+        [Description("Charlatan")]
+        charlatan,
+        [Description("Criminal")]
+        criminal,
+        [Description("Entertainer")]
+        entertainer,
+        [Description("Folk Hero")]
+        folkHero,
+        [Description("Guild Artisan")]
+        guildArtisan,
+        [Description("Guild Merchant")]
+        guildMerchant,
+        [Description("Hermit")]
+        hermit,
+        [Description("Knight")]
+        knight,
+        [Description("Noble")]
+        noble,
+        [Description("Outlander")]
+        outlander,
+        [Description("Pirate")]
+        pirate,
+        [Description("Sage")]
+        sage,
+        [Description("Sailor")]
+        sailor,
+        [Description("Soldier")]
+        soldier,
+        [Description("Spy")]
+        spy,
+        [Description("Urchin")]
+        urchin
+    }
+}
---
+++ DnD5E/Enums/ClassEnum.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DnD5E.Enums
+{
+    public enum ClassEnum
+    {
+        [Description("Barbarian")]
+        barbarian,
+        [Description("Bard")]
+        bard,
+        [Description("Cleric")]
+        cleric,
+        [Description("Druid")]
+        druid,
+        [Description("Fighter")]
+        fighter,
+        [Description("Monk")]
+        monk,
+        [Description("Paladin")]
+        paladin,
+        [Description("Ranger")]
+        ranger,
+        [Description("Rogue")]
+        rogue,
+        [Description("Sorceror")]
+        sorceror,
+        [Description("Warlock")]
+        warlock,
+        [Description("Wizard")]
+        wizard
+    }
+}
